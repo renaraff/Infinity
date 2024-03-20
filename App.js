3198a517c4d2d0cf7985b59ca08b152261daa0a8
renaraff <refurtado2007@gmail.com>
2024-03-20 09:42:09 -0300

    Inicio do projeto

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ assets/src/home.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+export default function Home( {navigation} )
+{
+    return (
+        <>
+        <View>
+
+        </View>
+        </>
+    )
+}
